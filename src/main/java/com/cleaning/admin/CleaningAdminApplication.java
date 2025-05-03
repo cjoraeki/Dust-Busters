b94cb70a4fed5bd4bf236d4388dc5cf888077491
cjoraeki <cjoraeki3@gmail.com>
2025-05-04 00:08:27 +0100

    render.yaml to deploy

--- src/main/java/com/cleaning/admin/CleaningAdminApplication.java
+++ src/main/java/com/cleaning/admin/CleaningAdminApplication.java
@@ -7,10 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CleaningAdminApplication {
 
 	public static void main(String[] args) {
-		String port = System.getenv("PORT");
-		if (port == null) {
-			port = "8080"; // default for local development
-		}
 		SpringApplication.run(CleaningAdminApplication.class, args);
 	}
 
